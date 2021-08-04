8a65828bfd1ac0cacd600003f360c511558c188e
Jesus <you@example.com>
2021-08-04 18:13:43 -0500

    arguments: returning a Job by ID

---
+++ server/resolvers.js
@@ -0,0 +1,15 @@
+const db = require('./db')
+const Query = {
+    job: (root, args) => {
+        console.log(root);
+        db.jobs.get(args.id)
+    },
+    jobs: () => db.jobs.list()
+}
+const Job = {
+    company: (job) => db.companies.get(job.companyId)
+}
+module.exports = {
+    Query,
+    Job
+}
