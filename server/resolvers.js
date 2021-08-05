f67e3457c8d17c3e67067ad334cf7d1a0c0f85bd
Jesus <you@example.com>
2021-08-04 20:13:37 -0500

    Queries finished

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,12 +1,18 @@
 const db = require('./db')
 const Query = {
-    job: (root, args) => db.jobs.get(args.id),
-    jobs: () => db.jobs.list()
+  company: (root, {id}) => db.companies.get(id),
+  job: (root, args) => db.jobs.get(args.id),
+  jobs: () => db.jobs.list()
+}
+const Company = {
+  jobs: (company) => db.jobs.list()
+  .filter(job => job.companyId === company.id)
 }
 const Job = {
-    company: (job) => db.companies.get(job.companyId)
+  company: (job) => db.companies.get(job.companyId)
 }
 module.exports = {
-    Query,
-    Job
+  Query,
+  Company,
+  Job
 }
